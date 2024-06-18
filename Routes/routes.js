5c64fffba6e5cf1d02f9e8213abd71200fd6eccb
Pancreasz <pummasinpp@gmai.com>
2024-06-18 16:08:40 +0700

    refactor_route

--- Routes/routes.js
+++ Routes/routes.js
@@ -10,113 +10,46 @@ const { ensureAuthenticated,
     facebook_auth,
     facebook_callback
 } = require('../login_sys/main.js');
-const { db, closedb } = require('../database/db_main.js');
-const path = require('path')
-const fs = require('fs')
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const otpGenerator = require('otp-generator');
+
+const {
+    sendImage,
+    renderProfile,
+    profileUpdate,
+    renderRegister,
+    renderLogin,
+    loginSubmit,
+    renderOtpverify,
+    otpVerify,
+    renderFailLogin,
+    imageUpload,
+    UploadProfileImg,
+    logout
+} = require('../controller/main_control.js')
+
 
 const router = express.Router();
 
-
-router.get('/image/:name', async (req, res) => {
-    const img = await utils.getProfileImageByName(req.params.name)
-    if (!img) {
-        res.sendFile(path.join(__dirname, `../public/default.jpg`))
-    } else {
-        res.sendFile(path.join(__dirname, `../public/${img.images_name}`))
-    }
-
-})
+router.get('/image/:name', sendImage)
 
 router.get('/', (req, res) => {
     res.send('cpre888 บิดแล้วรวยซวยแล้วมึง');
-
 });
 
-router.get('/profile/:name', ensureAuthenticated, async (req, res) => {
-    const data = await utils.getCustomerDataByName(req.params.name)
+router.get('/profile/:name', ensureAuthenticated, renderProfile);
 
-    // console.log(data)
-    const firstname = data.firstname;
-    const lastname = data.lastname;
+router.post('/profile', profileUpdate );
 
-    const name = req.params.name
+router.get('/register', renderRegister);
 
+router.get('/login', renderLogin);
 
-    if (req.params.name == req.user.username) {
-        res.render('profile', { firstname, lastname, name });
-    } else {
-        res.render('other_profile', { firstname, lastname, name })
-    }
+router.post('/submit', loginSubmit );
 
+router.get('/register/verify', renderOtpverify )
 
-});
+router.post('/verify-otp', otpVerify );
 
-
-
-router.post('/profile', async (req, res) => {
-    const formdata = req.body;
-    try {
-        await utils.updateData(formdata, req.user.id);
-        res.status(200).json({ redirect: `/profile/${req.user.username}` });
-    } catch (error) {
-        console.error('Error updating data:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
-
-
-router.get('/register', (req, res) => {
-    const prompt = 'register';
-    const command = 'submit';
-    const button = 'submit';
-    res.render('login_regis', { prompt, command, button });
-});
-
-router.get('/login', (req, res) => {
-    const prompt = 'login';
-    const command = 'login';
-    const button = 'login';
-    res.render('login_regis', { prompt, command, button });
-});
-
-
-
-router.post('/submit', async (req, res) => {
-    req.session.email = req.body.username
-    req.session.password = req.body.password
-
-    const otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
-    req.session.otp = otp;
-    console.log(otp)
-
-    sendOtpEmail(req.body.username, otp);
-    res.redirect('/register/verify')
-
-});
-
-router.get('/register/verify', async (req, res) => {
-    res.render('otp_verify')
-
-})
-
-router.post('/verify-otp', async (req, res) => {
-    const otp = await req.body.otp;
-    
-    if (req.session.otp == otp) {
-      res.redirect('/login');
-    } else {
-      res.send('Invalid OTP.');
-    }
-  });
-
-router.get('/fail', (req, res) => {
-    res.send('login failed.')
-})
-
+router.get('/fail', renderFailLogin )
 
 router.post('/login', authenticatedUser);
 
@@ -128,26 +61,8 @@ router.post('/auth_facebook', facebook_auth)
 
 router.get('/auth_facebook/callback', facebook_callback, profile_redirect);
 
+router.post('/upload', imageUpload , UploadProfileImg );
 
-router.post('/upload', utils.upload.single('image'), async (req, res) => {
-try {
-
-    utils.uploadProfileImage(req.file.filename, req.user.id)
-
-    // console.log(req.file);
-    console.log('File uploaded successfully')
-    res.redirect(`/profile/${req.user.username}`)
-
-} catch (error) {
-    res.status(500).send('Error uploading file');
-}
-});
-
-router.get('/logout', (req, res) => {
-    req.logout((err) => {
-      if (err) { return next(err); }
-      res.redirect('/login');
-    });
-  });
+router.get('/logout', logout );
 
 module.exports = router;
--- controller/main_control.js
+++ controller/main_control.js
@@ -1,4 +1,6 @@
 const utils = require('../utils/utils.js');
+const otpGenerator = require('otp-generator');
+const path = require('path')
 
 
 async function sendImage (req, res) {
@@ -9,4 +11,117 @@ async function sendImage (req, res) {
         res.sendFile(path.join(__dirname, `../public/${img.images_name}`))
     }
 
+}
+
+async function renderProfile (req, res) {
+    const data = await utils.getCustomerDataByName(req.params.name)
+
+    // console.log(data)
+    const firstname = data.firstname;
+    const lastname = data.lastname;
+
+    const name = req.params.name
+
+
+    if (req.params.name == req.user.username) {
+        res.render('profile', { firstname, lastname, name });
+    } else {
+        res.render('other_profile', { firstname, lastname, name })
+    }
+
+}
+
+
+async function profileUpdate (req, res) {
+    const formdata = req.body;
+    try {
+        await utils.updateData(formdata, req.user.id);
+        res.status(200).json({ redirect: `/profile/${req.user.username}` });
+    } catch (error) {
+        console.error('Error updating data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+function renderRegister (req, res) {
+    const prompt = 'register';
+    const command = 'submit';
+    const button = 'submit';
+    res.render('login_regis', { prompt, command, button });
+}
+
+function renderLogin (req, res) {
+    const prompt = 'login';
+    const command = 'login';
+    const button = 'login';
+    res.render('login_regis', { prompt, command, button });
+}
+
+async function loginSubmit (req, res) {
+    req.session.email = req.body.username
+    req.session.password = req.body.password
+
+    const otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
+    req.session.otp = otp;
+    console.log(otp)
+
+    sendOtpEmail(req.body.username, otp);
+    res.redirect('/register/verify')
+
+}
+
+function renderOtpverify (req, res) {
+    res.render('otp_verify')
+}
+
+async function otpVerify (req, res) {
+    const otp = await req.body.otp;
+    
+    if (req.session.otp == otp) {
+      res.redirect('/login');
+    } else {
+      res.send('Invalid OTP.');
+    }
+}
+
+function renderFailLogin (req, res) {
+    res.send('login failed.')
+}
+
+const imageUpload  = utils.upload.single('image')
+
+async function UploadProfileImg (req, res) {
+    try {
+    
+        utils.uploadProfileImage(req.file.filename, req.user.id)
+    
+        // console.log(req.file);
+        console.log('File uploaded successfully')
+        res.redirect(`/profile/${req.user.username}`)
+    
+    } catch (error) {
+        res.status(500).send('Error uploading file');
+    }
+}
+
+function logout (req, res) {
+    req.logout((err) => {
+      if (err) { return next(err); }
+      res.redirect('/login');
+    });
+}
+
+module.exports = {
+    sendImage,
+    renderProfile,
+    profileUpdate,
+    renderRegister,
+    renderLogin,
+    loginSubmit,
+    renderOtpverify,
+    otpVerify,
+    renderFailLogin,
+    imageUpload,
+    UploadProfileImg,
+    logout
 }
